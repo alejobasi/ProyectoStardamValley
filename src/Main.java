a959692e5ab1262fe9ebd7fe18994c8dc3d94773
Alejo Basilio Alfonso <basilioalfonsoalejo@gmail.com>
2024-10-16 13:49:38 +0200

    semillas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        Granja granja = new Granja();
+juego();
+
+
+    }
+
+    public static void juego(){
+
+        System.out.println("  BIENVENIDO A STARDAM VALLEY\n" +
+                         "-------------------------------");
+
+    }
+
+    public static void comprobarPartidaExiste(){
 
     }
 }
--- src/Tienda.java
+++ src/Tienda.java
@@ -1,3 +1,15 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Tienda {
@@ -5,7 +17,49 @@ public class Tienda {
     List<Semillas> listaTienda;
 
 
+    public Tienda() {
+        this.listaTienda = new ArrayList<>();
+    }
+    public static String RUTA_FICHERO_XML="resources"+File.separator+"semillas.xml";
+
+    public void cargarSemillas(){
+
+        try {
+            DocumentBuilderFactory dbFactory= DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBulider  = dbFactory.newDocumentBuilder();
+            Document doc= dBulider.parse(new File(RUTA_FICHERO_XML));
+            doc.getDocumentElement().normalize();
+
+            NodeList listaSemillas=doc.getElementsByTagName("semilla");
+
+            for (int i=0; i<listaSemillas.getLength();i++){
+                Node nodoSemilla=listaSemillas.item(i);
+                nodoSemilla.getAttributes();
+
+                if (nodoSemilla.getNodeType()==Node.ELEMENT_NODE){
+                    Element semilla=(Element) nodoSemilla;
+
+                    int id= Integer.parseInt(semilla.getAttribute("id"));
+                    String nombre=semilla.getElementsByTagName("nombre").item(0).getTextContent();
+                    String estacion=semilla.getElementsByTagName("estacion").item(0).getTextContent();
+                    int diasCrecimiento= Integer.parseInt(semilla.getElementsByTagName("diasCrecimiento").item(0).getTextContent());
+                    int precio= Integer.parseInt(semilla.getElementsByTagName("precioCompraSemilla").item(0).getTextContent());
+                    int cantidad= Integer.parseInt(semilla.getElementsByTagName("maxFrutos").item(0).getTextContent());
+
+                    Semillas sem= new Semillas(id,nombre,estacion,diasCrecimiento,precio,cantidad);
+
+                    listaTienda.add(sem);
+
+                }
+
+            }
 
 
+        } catch (SAXException e) {
+            throw new RuntimeException(e);
+        } catch (IOException | ParserConfigurationException e) {
+            throw new RuntimeException(e);
+        }
 
+    }
 }
