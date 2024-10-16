e16931afa54189989e3f38d59b3cc3fc421cbfc5
Alejo Basilio Alfonso <basilioalfonsoalejo@gmail.com>
2024-10-16 15:03:32 +0200

    CARGAR PARTIDA

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class Main {
+        public static String RUTA_JUEGO="resources"+ File.separator+"StardamValley.bin";
     public static void main(String[] args) {
         Granja granja = new Granja();
 juego();
@@ -12,10 +17,23 @@ juego();
 
         System.out.println("  BIENVENIDO A STARDAM VALLEY\n" +
                          "-------------------------------");
+        System.out.println("1. NUEVA PARTIDA");
+        if (comprobarPartidaExiste()){
+
+            System.out.println("2. CARGAR PARTIDA");
+
+        }
 
     }
 
-    public static void comprobarPartidaExiste(){
+    public static boolean comprobarPartidaExiste(){
 
+        Path path= Paths.get(RUTA_JUEGO);
+        if (Files.exists(path)){
+            return true;
+        }else {
+
+            return false;
+        }
     }
 }
